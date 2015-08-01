383938fca4915bf2a2c62e347b3715a0a7694ae8
Spepsi <floriangardin@hotmail.fr>
2015-08-01 22:50:10 +0100

    add constructors

--- src/model/ActionObjet.java
+++ src/model/ActionObjet.java
@@ -2,4 +2,18 @@ package model;
 
 public abstract class ActionObjet extends Objet{
 
+	protected float vx;
+	protected float vy;
+	protected int team;
+	protected Objet target;
+	
+public void action(){
+	
+	
+}
+public void move(){
+	
+}
+	
+	
 }
--- src/model/Character.java
+++ src/model/Character.java
@@ -2,4 +2,9 @@ package model;
 
 public class Character extends ActionObjet{
 
+	protected Circle sightBox;
+	protected Weapon weapon;
+	protected Armor armor;
+	
+	
 }
