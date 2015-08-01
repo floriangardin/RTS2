8e35c112e45c5245dff8a9b5702b4dd14af133b0
Kévin Bienvenu <bienvenu.kevin@sfr.fr>
2015-08-01 23:44:28 +0200

    new objet

--- src/model/Objet.java
+++ src/model/Objet.java
@@ -1,5 +1,25 @@
 package model;
 
-public abstract class Objet {
+import java.util.Vector;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
 
+public abstract class Objet {
+	
+	protected float x;
+	protected float y;
+	protected Shape collisionBox;
+	protected Color color;
+	protected Plateau p;
+	protected float lifePoints;
+	
+	protected void destroy(){
+		this.lifePoints = -10;
+	}
+	protected void draw(){}
+	protected void collision(Objet o){}
+	
 }
--- src/model/RidableObjet.java
+++ src/model/RidableObjet.java
@@ -1,6 +1,11 @@
 package model;
 
 public abstract class RidableObjet extends Objet {
-
 	// A ridable objet doesn't do any action ....
+	protected float velocity;
+
+	public RidableObjet(float velocity) {
+		super();
+		this.velocity = velocity;
+	}
 }
