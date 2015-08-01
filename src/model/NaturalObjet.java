e9f175b4698ad86a6d14cd0bb13b528900560f06
Kévin Bienvenu <bienvenu.kevin@sfr.fr>
2015-08-01 23:55:13 +0200

    new start

--- src/model/NaturalObjet.java
+++ src/model/NaturalObjet.java
@@ -1,5 +1,13 @@
 package model;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.geom.Shape;
+
 public abstract class NaturalObjet extends Objet {
+
+	public NaturalObjet(float x, float y, Shape collisionBox, Color color, Plateau p, float lifePoints) {
+		super(x, y, collisionBox, color, p, lifePoints);
+		// TODO Auto-generated constructor stub
+	}
 
 }
