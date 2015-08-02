1e533227ee95f3ca813ddcf995a926853b7b3456
Spepsi <floriangardin@hotmail.fr>
2015-08-02 15:13:53 +0100

    add rock and water

--- src/model/Water.java
+++ src/model/Water.java
@@ -1,15 +1,17 @@
 package model;
 
 import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.geom.Rectangle;
 
 public class Water extends NaturalObjet {
 
 	public Water(float x, float y, Plateau p) {
-		float size = 1.0f;
+		float size = 50.0f;
 		this.collisionBox = new Rectangle(x-size/2,y-size/2,size,size);
 		this.color = Color.blue;
 		this.p = p;
+		p.addNaturalObjets(this);
 		this.lifePoints = 1.0f;
 		this.setXY(x, y);
 	}
@@ -18,8 +20,9 @@ public class Water extends NaturalObjet {
 		//TODO: collision
 	}
 	
-	public void draw(Objet o){
-		//TODO: draw according to orientation of owner
+	public Graphics draw(Graphics g){
+		g.setColor(this.color);
+		g.fill(this.collisionBox);
+		return g;
 	}
-
 }
