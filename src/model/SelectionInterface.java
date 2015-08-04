d7035193e74a4e6dbc9f65bc42fbba0ac57eaad6
Spepsi <floriangardin@hotmail.fr>
2015-08-04 23:41:53 +0100

    hoverboard

--- src/model/SelectionInterface.java
+++ src/model/SelectionInterface.java
@@ -1,6 +1,8 @@
 package model;
 
-import java.awt.Graphics;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+
 
 public class SelectionInterface extends Bar {
 
@@ -8,10 +10,27 @@ public class SelectionInterface extends Bar {
 	
 	public SelectionInterface(BottomBar parent){
 		this.parent = parent;
+		this.sizeX = parent.sizeX/3f;
+		this.sizeY = parent.sizeY;
+		this.x = parent.x+parent.sizeX/3f;
+		this.y = parent.y;
 	}
 	
 	public Graphics draw(Graphics g){
+		
 		// Draw the selection of current player
+		int compteurX = 0;
+		int compteurY = 0;
+		float startX = this.x;
+		float startY = this.y;
+		g.setColor(Color.red);
+		for(Character c : this.parent.player.selection){
+			g.drawString("C",startX+10f*compteurX,startY+10f*compteurY);
+			compteurX++;
+			if(compteurX>2){
+				compteurY++;
+			}
+		}
 		return g;
 	}
 }
