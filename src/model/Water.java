d34b452efe0f0463ff0b5a7afeb8c380e1b9920a
Spepsi <floriangardin@hotmail.fr>
2015-08-04 22:19:07 +0100

    add sand and water

--- src/model/Water.java
+++ src/model/Water.java
@@ -2,12 +2,20 @@ package model;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 
 public class Water extends NaturalObjet {
-
+	
 	public Water(float x, float y, Plateau p) {
-		float size = 50.0f;
+		float size = 32.0f;
+		try {
+			this.image= new Image("pics/water.jpg");
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		this.collisionBox = new Rectangle(x-size/2,y-size/2,size,size);
 		this.color = Color.blue;
 		this.p = p;
@@ -21,8 +29,7 @@ public class Water extends NaturalObjet {
 	}
 	
 	public Graphics draw(Graphics g){
-		g.setColor(this.color);
-		g.fill(this.collisionBox);
+		g.drawImage(this.image,this.collisionBox.getX(),this.collisionBox.getY());
 		return g;
 	}
 }
