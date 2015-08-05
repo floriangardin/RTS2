f90e15e11f41d3b9350c4bcc0ea1b320147d3a06
Spepsi <floriangardin@hotmail.fr>
2015-08-05 20:58:27 +0100

    add difficulty

--- src/model/EnnemyGenerator.java
+++ src/model/EnnemyGenerator.java
@@ -3,11 +3,12 @@ package model;
 import org.newdawn.slick.geom.Point;
 
 public class EnnemyGenerator extends ActionObjet {
-// Class which pop ennemies at random time interval
+	// Class which pop ennemies at random time interval
 
 	Game g;
 	float spawnTime;
 	float state;
+	int n_generated ;
 	public EnnemyGenerator(Plateau p,Game g,float x, float y){
 		p.addEquipmentObjets(this);
 		this.x = x;
@@ -20,22 +21,52 @@ public class EnnemyGenerator extends ActionObjet {
 		this.lifePoints = 1f;
 		this.collisionBox= new Point(x,y);
 	}
-	
+
 	public void action(){
 		state+=0.1f;
 		if(state>=this.spawnTime){
 			this.generate();
 			this.state=0;
+			n_generated++;
 		}
+
 	}
 	public void generate(){
-		
+
 		// Create character
-		
+
 		Character c = new Character(p,team,x+((float)Math.random()-0.5f)*40f,y+((float)Math.random()-0.5f)*40f);
 		// Add a random weapon to character
-		c.collectArmor(new LightArmor(c.getX(),c.getY(),p,c));
-		c.collectWeapon(new Sword(p,c));
-		
+		if(n_generated<10){
+			c.collectArmor(new LightArmor(c.getX(),c.getY(),p,c));
+			c.collectWeapon(new Sword(p,c));	
+		}
+		else if(n_generated<40){
+			if(n_generated%2==0){
+				c.collectArmor(new LightArmor(c.getX(),c.getY(),p,c));
+				c.collectWeapon(new Bow(p,c));
+			}
+			else{
+				c.collectArmor(new MediumArmor(c.getX(),c.getY(),p,c));
+				c.collectWeapon(new Sword(p,c));	
+			}
+		}
+		else if(n_generated<80){
+			if(n_generated%3==0){
+				c.collectArmor(new MediumArmor(c.getX(),c.getY(),p,c));
+				c.collectWeapon(new Balista(p,c));
+			}
+			else if(n_generated%3==1){
+				c.collectArmor(new MediumArmor(c.getX(),c.getY(),p,c));
+				c.collectWeapon(new Sword(p,c));	
+			}
+			else{
+				c.collectArmor(new MediumArmor(c.getX(),c.getY(),p,c));
+				c.collectWeapon(new Bow(p,c));
+			}
+		}
+
+
+
 	}
 }
