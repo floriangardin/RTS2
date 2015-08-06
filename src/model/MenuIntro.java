81323721ca7f573b0f7e8ffb2823f094fd64eb6b
Kévin Bienvenu <bienvenu.kevin@sfr.fr>
2015-08-06 21:32:20 +0200

    e

--- src/model/MenuIntro.java
+++ src/model/MenuIntro.java
@@ -16,6 +16,9 @@ public class MenuIntro extends Menu {
 	float nextBullet = 1f;
 	Image title;
 
+	boolean toGame = false;
+	public float timeToGame = 200f;
+
 	public MenuIntro(Game game){
 		this.game = game;
 		this.items = this.createHorizontalCentered(2, this.game.resX, this.game.resY);
@@ -50,8 +53,7 @@ public class MenuIntro extends Menu {
 	public void callItem(int i){
 		switch(i){
 		case 0:
-			this.game.newGame();
-			this.game.quitMenu();
+			this.toGame = true;
 			break;
 		case 1: 
 			this.game.app.exit();
@@ -61,86 +63,106 @@ public class MenuIntro extends Menu {
 	}
 
 	public void draw(Graphics g){
-		
-
-		for(int i=0;i<3;i++){
-			for(int j=0;j<2;j++){
-				g.drawImage(this.game.background, i*512, j*512);
+		if(toGame){
+			if(this.timeToGame>80f){
+				g.setColor(new Color(0f,0f,0f,0.05f));
+			} else {
+				if(this.timeToGame>50f){
+					g.setColor(new Color(0f,0f,0f,0.5f));
+				} else {
+					g.setColor(new Color(0f,0f,0f,0.9f));
+					
+				}
 			}
+			g.fillRect(0, 0, this.game.resX, this.game.resY);
+		} else {
+			for(int i=0;i<3;i++){
+				for(int j=0;j<2;j++){
+					g.drawImage(this.game.background, i*512, j*512);
+				}
+			}
+			Vector<Objet> toDraw = new Vector<Objet>();
+			for(Objet o: this.trees)
+				toDraw.add(o);
+			for(Bullet b: this.bullets)
+				toDraw.add(b);
+			Utils.triY(toDraw);
+			for(Objet o: toDraw){
+				o.draw(g);
+			}
+			for(int i=0; i<this.items.size(); i++){
+				this.items.get(i).draw(g);
+			}
+			g.drawImage(this.title, this.game.resX/2f-200f, 20f);
+			g.setColor(Color.black);
+			String copyright = "           Copyright 2015           \n- GdB Production / Welcome's Games -";
+			g.drawString(copyright, 6.6f*this.game.resX/18f, 4f*this.game.resY/5f);
+			//            abcdefghijklmn  abcdefghijklmnopqrstuvwxyabcdefghijk
 		}
-		Vector<Objet> toDraw = new Vector<Objet>();
-		for(Objet o: this.trees)
-			toDraw.add(o);
-		for(Bullet b: this.bullets)
-			toDraw.add(b);
-		Utils.triY(toDraw);
-		for(Objet o: toDraw){
-			o.draw(g);
-		}
-		for(int i=0; i<this.items.size(); i++){
-			this.items.get(i).draw(g);
-		}
-		g.drawImage(this.title, this.game.resX/2f-200f, 20f);
-		g.setColor(Color.black);
-		String copyright = "           Copyright 2015           \n- GdB Production / Welcome's Games -";
-		g.drawString(copyright, 6.6f*this.game.resX/18f, 4f*this.game.resY/5f);
-		//            abcdefghijklmn  abcdefghijklmnopqrstuvwxyabcdefghijk
 	}
 
 	public void update(Input i){
-		if(i.isMousePressed(Input.MOUSE_LEFT_BUTTON))
-			callItems(i);
-		if(i.isKeyPressed(Input.KEY_R)){
-			this.game.app.setShowFPS(true);
-		}
-		for(Menu_Item item: this.items)
-			item.update(i);
-		this.timer += 0.1f;
-		for(Bullet b : this.bullets)
-			b.action();
-		if(this.timer>this.nextBullet){
-			this.timer = 0f;
-			float x1 = (float)(Math.random()*this.game.resX), x2 = (float)(Math.random()*this.game.resX);
-			float y1 = (float)(Math.random()*this.game.resY), y2 = (float)(Math.random()*this.game.resY);
-			boolean arrow = Math.random()>0.5;
-			if(x1/this.game.resX>y1/this.game.resY){
-				if(x1/this.game.resX+y1/this.game.resY>1f){
-					y1 = this.game.resY;
-				} else {
-					x1 = 0f;
-				}
-			} else {
-				if(x1/this.game.resX+y1/this.game.resY>1f){
-					x1 = this.game.resX;
-				} else {
-					y1 = 0f;
-				}
+		if(!toGame){
+			if(i.isMousePressed(Input.MOUSE_LEFT_BUTTON))
+				callItems(i);
+			if(i.isKeyPressed(Input.KEY_R)){
+				this.game.app.setShowFPS(true);
 			}
-			if(arrow){
-				if(x2/this.game.resX>y2/this.game.resY){
-					if(x2/this.game.resX+y2/this.game.resY>1f){
-						y2 = this.game.resY;
+			for(Menu_Item item: this.items)
+				item.update(i);
+			this.timer += 0.1f;
+			for(Bullet b : this.bullets)
+				b.action();
+			if(this.timer>this.nextBullet){
+				this.timer = 0f;
+				float x1 = (float)(Math.random()*this.game.resX), x2 = (float)(Math.random()*this.game.resX);
+				float y1 = (float)(Math.random()*this.game.resY), y2 = (float)(Math.random()*this.game.resY);
+				boolean arrow = Math.random()>0.5;
+				if(x1/this.game.resX>y1/this.game.resY){
+					if(x1/this.game.resX+y1/this.game.resY>1f){
+						y1 = this.game.resY;
 					} else {
-						x2 = 0f;
+						x1 = 0f;
 					}
 				} else {
-					if(x2/this.game.resX+y2/this.game.resY>1f){
-						x2 = this.game.resX;
+					if(x1/this.game.resX+y1/this.game.resY>1f){
+						x1 = this.game.resX;
 					} else {
-						y2 = 0f;
+						y1 = 0f;
 					}
 				}
-				this.bullets.addElement(new MenuArrow(x1,x2,y1,y2,this));
-			} else {
-				this.bullets.addElement(new MenuFireball(x1,y1,x2,y2,this));
+				if(arrow){
+					if(x2/this.game.resX>y2/this.game.resY){
+						if(x2/this.game.resX+y2/this.game.resY>1f){
+							y2 = this.game.resY;
+						} else {
+							x2 = 0f;
+						}
+					} else {
+						if(x2/this.game.resX+y2/this.game.resY>1f){
+							x2 = this.game.resX;
+						} else {
+							y2 = 0f;
+						}
+					}
+					this.bullets.addElement(new MenuArrow(x1,x2,y1,y2,this));
+				} else {
+					this.bullets.addElement(new MenuFireball(x1,y1,x2,y2,this));
+				}
+			}
+			Vector<Bullet> toremove = new Vector<Bullet>();
+			for(Bullet b: this.bullets){
+				if(b.lifePoints<=0)
+					toremove.add(b);
+			}
+			for(Bullet b: toremove)
+				this.bullets.remove(b);
+		} else {
+			this.timeToGame -= 1f;
+			if(timeToGame<0f){
+				this.game.newGame();
+				this.game.quitMenu();
 			}
 		}
-		Vector<Bullet> toremove = new Vector<Bullet>();
-		for(Bullet b: this.bullets){
-			if(b.lifePoints<=0)
-				toremove.add(b);
-		}
-		for(Bullet b: toremove)
-			this.bullets.remove(b);
 	}
 }
