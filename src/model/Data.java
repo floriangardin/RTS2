88687f109c981ed7d3944ad3758f51eb9fe1638f
MagicWelcome <bienvenu.kevin@sfr.fr>
2015-09-12 22:37:26 +0200

    mythe

--- src/model/Data.java
+++ src/model/Data.java
@@ -86,7 +86,7 @@ public class Data {
 		
 	public float bonusSpearHorse = 3f;
 	public float bonusSwordBow = 3f;
-	public float bonusBowFoot = 2f;
+	public float bonusBowFoot = 3f;
 	public float bonusWandBow = 2f;
 	
 	// Ressources
