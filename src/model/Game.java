63ea1dbc5d889ed0f5fa69160c15b18973eb9256
MagicWelcome <bienvenu.kevin@sfr.fr>
2015-09-19 17:24:22 +0200

    test multi

--- src/model/Game.java
+++ src/model/Game.java
@@ -144,7 +144,7 @@ public class Game extends BasicGame
 		}
 
 		g.setColor(Color.black);
-		g.fillRect(this.plateau.maxX, 0, this.plateau.maxX, this.plateau.maxY);
+		g.fillRect(this.plateau.maxX, 0, this.plateau.maxX, this.plateau.maxY*2);
 		g.fillRect(0, this.plateau.maxY, this.plateau.maxX, this.plateau.maxY);
 		//g.fillRect(0,0,gc.getScreenWidth(),gc.getScreenHeight());
 
