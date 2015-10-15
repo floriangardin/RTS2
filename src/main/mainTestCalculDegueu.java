dd598eb3c8ef85774871884f985e2eba8a22b386
MagicWelcome <bienvenu.kevin@sfr.fr>
2015-10-15 17:52:41 +0200

    euh ?

--- src/main/mainTestCalculDegueu.java
+++ src/main/mainTestCalculDegueu.java
@@ -6,7 +6,11 @@ public class mainTestCalculDegueu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		String s = "b c e ";
+		String[] a = s.split(" ");
+		for(int i=0; i<a.length; i++){
+			System.out.println("ligne:"+a[i]);
+		}
 
 	}
 
