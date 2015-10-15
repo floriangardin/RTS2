39ece13c69eab4ff2372167fa230ab352e72fbf1
MagicWelcome <bienvenu.kevin@sfr.fr>
2015-10-15 16:58:24 +0200

    euh ?

--- src/model/Map.java
+++ src/model/Map.java
@@ -126,7 +126,7 @@ public class Map {
 		data1.player.gold = 1000;
 		data1.player.food = 1000;
 		Character c= data2.player.create(UnitsList.Crossbowman, 7*X/9 - 1f, Y/2);
-		c.ia = new IAUnit(c,Utils.loadFloatMatrix("././IA/MicroSimulator/matrix_0007.txt"));
+		//c.ia = new IAUnit(c,Utils.loadFloatMatrix("././IA/MicroSimulator/matrix_0007.txt"));
 		
 		// Player 2 side
 
