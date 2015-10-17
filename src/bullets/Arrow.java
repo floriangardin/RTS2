f0f762e39a9f0d091d0710ba4713a275e74a88a8
MagicWelcome <bienvenu.kevin@sfr.fr>
2015-10-17 18:42:17 +0200

    multi gne

--- src/bullets/Arrow.java
+++ src/bullets/Arrow.java
@@ -24,10 +24,9 @@ public class Arrow extends Bullet{
 		// 
 		this.p = p;
 		if(id==-1){
-			this.id = p.g.idBullet;
-			p.g.idBullet++;
-		}
-		else{
+			this.id = p.g.idChar;
+			p.g.idChar++;
+		}else{
 			this.id=id;
 		}
 		this.name ="arrow";
@@ -55,26 +54,7 @@ public class Arrow extends Bullet{
 		this.sound = p.g.sounds.arrow;
 		this.sound.play(1f,this.p.g.options.soundVolume);
 	}
-	public Arrow(OutputBullet ocb ,Plateau p){
-		// Only used to display on client screen
-		// Parameters
-		this.p = p;
-		this.id = ocb.id;
-		p.addBulletObjets(this);
-		this.lifePoints = 1f;
-		this.collisionBox = new Point(x,y);
-		this.setXY(ocb.x,ocb.y);
-		this.vx = ocb.vx;
-		this.vy = ocb.vy;
-		this.angle = (float) (Math.atan(vy/(vx+0.00001f))*180/Math.PI);
-		if(this.vx<0)
-			this.angle+=180;
-		if(this.angle<0)
-			this.angle+=360;
-		this.image = p.g.images.arrow.getScaledCopy(1f);
-		this.image.rotate(this.angle);
-	}
-
+	
 	public void collision(Character c){
 		if(c.team!=this.owner.team){
 			// Attack if armor<damage and collision
