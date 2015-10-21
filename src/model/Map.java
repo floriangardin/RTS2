9a2b32e0fa9d72f54f820d4ed351cd5c10c72340
Spepsi <floriangardin@hotmail.fr>
2015-10-22 00:13:48 +0200

    tech parsing

--- src/model/Map.java
+++ src/model/Map.java
@@ -114,8 +114,7 @@ public class Map {
 		new BuildingAcademy(game.plateau,game,2*game.plateau.maxX/6+40f,3*game.plateau.maxY/4+55f);
 		
 		// Water
-
-			
+		
 		// UbiSoft C'est Cool 
 			new Water(3.0f*game.plateau.maxX/6,175f,800f,350f,game.plateau);
 			new Water(3.0f*game.plateau.maxX/6,game.plateau.maxY-175f,800f,350f,game.plateau);
@@ -125,7 +124,7 @@ public class Map {
 		new BuildingMill(game.plateau,game,game.plateau.maxX-200f,1*game.plateau.maxY/4+55f);
 		new BuildingMine(game.plateau,game,game.plateau.maxX-200f,3*game.plateau.maxY/4-55f);
 		new BuildingBarrack(game.plateau,game,4*game.plateau.maxX/5-100f,game.plateau.maxY/2);
-		
+
 		data2.create(UnitsList.Spearman,  game.plateau.maxX-350f, game.plateau.maxY/2+3f);
 		
 		
@@ -182,12 +181,14 @@ public class Map {
 
 		new BuildingHeadQuarters(game.plateau,game,0f,0f,1);
 		new BuildingHeadQuarters(game.plateau,game,X-200f,0f,2);
+		new BuildingBarrack(game.plateau,game,0f,Y-200f);
+		new BuildingBarrack(game.plateau,game,X-200f,Y-200f);
 		
 		//data1.player.create(UnitsList.Spearman, 2*X/9 + (float)Math.random(), Y/2+(float)Math.random());
-		for(int i = 0;i<1;i++){
+		for(int i = 0;i<2;i++){
 			data1.player.create(UnitsList.Crossbowman, 2*X/9+i - 1f, Y/2);
+			data1.player.create(UnitsList.Spearman, 2*X/9+i - 1f, Y/2+1f);
 		}
-			
 		//data1.player.create(UnitsList.Spearman, X/9 + 2f, Y/2);
 		data1.player.gold = 1000;
 		data1.player.food = 1000;
@@ -195,10 +196,10 @@ public class Map {
 		data2.player.gold = 1000;
 		data2.player.food = 1000;
 		
-		for(int i = 0;i<1;i++){
+		for(int i = 0;i<2;i++){
 			data2.player.create(UnitsList.Crossbowman, 7*X/9+i - 1f, Y/2);
+			data2.player.create(UnitsList.Spearman, 7*X/9+i - 1f, Y/2+1f);
 		}
-		
 		
 		// Player 2 side
 
@@ -247,7 +248,7 @@ public class Map {
 		
 		new BuildingAcademy(game.plateau,game,X/2,Y/2);
 
-		for(int caca =0; caca<20; caca++)
+		for(int c =0; c<20; c++)
 			data1.player.create(UnitsList.Spearman, X/9 + (float)Math.random(), Y/2+(float)Math.random());
 
 		//data1.player.create(UnitsList.Spearman, X/9 + 2f, Y/2);
