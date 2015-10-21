7f53a57b3176f2328e13cf6a2b485267f1c70e7b
MagicWelcome <bienvenu.kevin@sfr.fr>
2015-10-21 23:49:42 +0200

    e

--- src/model/Game.java
+++ src/model/Game.java
@@ -267,7 +267,7 @@ public class Game extends BasicGame
 	public void newGame(){
 		//Clean all variables
 
-		Map.createMap("techTest",this);
+		Map.createMap("testTech",this);
 
 		//System.out.println(this.plateau.mapGrid);
 		//			Map.createMapEmpty(this);
