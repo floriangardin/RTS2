9d401913f1e84f127f58723cde81b57190baf2a9
MagicWelcome <bienvenu.kevin@sfr.fr>
2015-10-22 00:04:25 +0200

    e

--- src/model/Game.java
+++ src/model/Game.java
@@ -241,6 +241,7 @@ public class Game extends BasicGame
 					if(outputs.size()>0){
 						this.plateau.currentString = outputs.lastElement();
 						outputs.remove(outputs.lastElement());
+						outputs.clear();
 						//System.out.println("paquets perdus:" +(outputs.size()-1));
 						//outputs.clear();
 					}
