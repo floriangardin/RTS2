433b1c259f2000c0d5b97f6f9bbdc566cd4257dc
MagicWelcome <bienvenu.kevin@sfr.fr>
2015-11-11 16:11:44 +0100

    communication prete pour le debug

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,15 +17,15 @@ public class Main {
 //		double width = 600;
 //		double height = 400;
 		System.setProperty("org.lwjgl.librarypath", new File(new File(System.getProperty("user.dir"), "native"), LWJGLUtil.getPlatformName()).getAbsolutePath());
-		int resolutionX = (int)screenSize.getWidth();		
-		int resolutionY = (int)screenSize.getHeight();
-//		int resolutionX = 800;		
-//		int resolutionY = 600;
+//		int resolutionX = (int)screenSize.getWidth();		
+//		int resolutionY = (int)screenSize.getHeight();
+		int resolutionX = 800;		
+		int resolutionY = 600;
 		try {
 			Game game = new Game(resolutionX,resolutionY);
 			AppGameContainer app = new AppGameContainer( game );
 			game.app = app;
-			app.setDisplayMode(resolutionX, resolutionY,true);
+//			app.setDisplayMode(resolutionX, resolutionY,true);
 //			app.setDisplayMode(resolutionX, resolutionY,false);
 			app.setTargetFrameRate(Main.framerate);
 			app.setVSync(true);
