d4f52198b1a3b7b03f434843a8df4b4246e751d5
Spepsi <floriangardin@hotmail.fr>
2015-11-11 17:45:52 +0100

    clock

--- src/main/mainTestCalculDegueu.java
+++ src/main/mainTestCalculDegueu.java
@@ -9,29 +9,7 @@ public class mainTestCalculDegueu {
 
 	public static void main(String[] args) {
 
-		
-		Shape c1 = new Circle(0,0,20);
-		Shape c2 = new Circle(15,0,20);
-		long time = System.currentTimeMillis();
-		boolean test;
-		for(int i=0; i<20000000; i++){
-			test = c1.intersects(c2);
-		}
-		System.out.println("intersects " + (System.currentTimeMillis()-time));
-		time = System.currentTimeMillis();
-		for(int i=0; i<20000000; i++){
-			test = Utils.distance(c1.getCenterX(), c1.getCenterY(), c2.getCenterX(), c2.getCenterY())<c1.getBoundingCircleRadius();
-		}
-		System.out.println("getbcr " + (System.currentTimeMillis()-time));
-		time = System.currentTimeMillis();
-		for(int i=0; i<20000000; i++){
-			test = Utils.distance(c1.getCenterX(), c1.getCenterY(), c2.getCenterX(), c2.getCenterY())<20;
-		}
-		System.out.println("getbcr " + (System.currentTimeMillis()-time));
-
-		
+		System.out.println(System.nanoTime());
 	}
-
-
 	
 }
