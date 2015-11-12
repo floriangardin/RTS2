36906569e9a022a9741526a7c6d7638c426d1301
Spepsi <floriangardin@hotmail.fr>
2015-11-12 04:12:35 +0100

    blofeld

--- src/multiplaying/MultiReceiver.java
+++ src/multiplaying/MultiReceiver.java
@@ -35,7 +35,7 @@ public class MultiReceiver extends Thread{
 			if(debug)
 				System.out.println("Cr�ation d'un receiver - " + port);
 			while(!server.isClosed()){
-				message = new byte[512];
+				message = new byte[300];
 				packet = new DatagramPacket(message, message.length);
 				try{
 					server.receive(packet);
