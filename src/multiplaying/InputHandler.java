d9a7ac041419e619260c183abb75676f804cf219
Spepsi <floriangardin@hotmail.fr>
2015-11-12 04:03:12 +0100

    blofeld

--- src/multiplaying/InputHandler.java
+++ src/multiplaying/InputHandler.java
@@ -39,18 +39,18 @@ public class InputHandler {
 		while(i<this.inputs.size()){
 			InputObject in = this.inputs.get(i);
 			//If right round and validated add it to player inputs to play
-			if(round==(in.round+2) && in.isValidated()  ){
+			if(round==(in.round+7) && in.isValidated()  ){
 				//ADD inputs in player
 				toReturn.add(in);
 				toRemove.add(in);
 			}
 			//If right round but not validated, erase the input
-			else if(this.g.round==(in.round+2) && !in.isValidated()){
+			else if(this.g.round==(in.round+7) && !in.isValidated()){
 				this.g.vroundDropped.addElement(in.id);
 				cause = 1;
 			}
 			//If too late to play this input, erase the input
-			else if(this.g.round>(in.round+2)){
+			else if(this.g.round>(in.round+7)){
 				toRemove.add(in);
 			}
 
