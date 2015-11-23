2caf666b3bd67a820cbcdb41fcae6816df4a8230
Spepsi <floriangardin@hotmail.fr>
2015-11-23 17:35:17 +0100

    add minimap

--- src/model/Game.java
+++ src/model/Game.java
@@ -394,7 +394,7 @@ public class Game extends BasicGame
 							if(host){
 								//Send restart process 
 								this.restartProcess = true;
-								this.timeRestart = this.clock.getCurrentTime()+(long)1e9;
+								this.timeRestart = this.clock.getCurrentTime()+(long)(0.1*1e9);
 								this.sendInputToAllPlayer("3K|"+this.timeRestart+"|");
 								//TODO : send message of resynch
 							}
