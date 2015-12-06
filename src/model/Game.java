b0ca33173ed6a7d312fd0e8120e05fc5f971d9d3
MagicWelcome <bienvenu.kevin@sfr.fr>
2015-12-06 23:37:24 +0100

    oh putain

--- src/model/Game.java
+++ src/model/Game.java
@@ -428,13 +428,13 @@ public class Game extends BasicGame
 		}
 		if(processSynchro){
 			g.setColor(Color.green);
-			g.drawString("Resynchro", 30f, 10f);
-			g.fillRect(10f,10f,15f,15f);
+			g.drawString("Resynchro", 30f, 90f);
+			g.fillRect(10f,90f,15f,15f);
 		}
 		if(antidropProcess){
 			g.setColor(Color.blue);
-			g.drawString("AntiDrop", 30f, 30f);
-			g.fillRect(10f,30f,15f,15f);
+			g.drawString("AntiDrop", 30f, 140f);
+			g.fillRect(10f,140f,15f,15f);
 		}
 		if(debugTimeSteps)
 			System.out.println("fin du render : "+(System.currentTimeMillis()-timeSteps));
