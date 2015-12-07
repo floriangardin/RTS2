d817ddd5de36ac942ee60e87a86d597094329dc5
Spepsi <floriangardin@hotmail.fr>
2015-12-07 04:46:04 +0100

    oh mon dieu

--- src/multiplaying/InputHandler.java
+++ src/multiplaying/InputHandler.java
@@ -11,7 +11,7 @@ import java.util.concurrent.locks.Lock;
 public class InputHandler {
 
 	private Vector<InputObject> inputs;
-	public static int nDelay=2;
+	public static int nDelay=10;
 	Game g;
 	public Lock mutex;
 
