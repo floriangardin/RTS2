719e03a2f9343cfc19fceff0a99f2622109c63a4
Spepsi <floriangardin@hotmail.fr>
2015-12-07 05:23:52 +0100

    champagne

--- src/multiplaying/InputHandler.java
+++ src/multiplaying/InputHandler.java
@@ -11,7 +11,7 @@ import java.util.concurrent.locks.Lock;
 public class InputHandler {
 
 	private Vector<InputObject> inputs;
-	public static int nDelay=3;
+	public static int nDelay=5;
 	Game g;
 	public Lock mutex;
 
