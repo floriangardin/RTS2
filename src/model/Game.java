be6efcdda6239b54448a7b0a274898ffea051bcb
Spepsi <floriangardin@hotmail.fr>
2015-12-07 02:28:42 +0100

    oh mon dieu

--- src/model/Game.java
+++ src/model/Game.java
@@ -44,7 +44,7 @@ public class Game extends BasicGame
 	public static boolean debugTimeSteps = false;
 	public static boolean debugPaquet = false;
 	public static boolean debugValidation = true;
-	public static boolean debugReceiver = false;
+	public static boolean debugReceiver = true;
 	public static boolean debugSender = true;
 	public static boolean debugTourEnCours = false;
 	public static boolean debugThread = false;
