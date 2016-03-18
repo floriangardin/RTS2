de6ba60675eec541694d9ead128a5a59bc71d957
Spepsi <floriangardin@hotmail.fr>
2016-03-18 22:07:05 +0100

    ctrl a  suppr

--- src/model/Game.java
+++ src/model/Game.java
@@ -76,7 +76,7 @@ public class Game extends BasicGame
 	public static boolean debugSender = false;
 	public static boolean debugTourEnCours = false;
 	public static boolean debugThread = false;
-	public static boolean debugDisplayDebug = false;
+	public static boolean debugDisplayDebug = true;
 
 	public static boolean deplacementGroupIntelligent = true;
 	public static boolean debugGroup = false;
