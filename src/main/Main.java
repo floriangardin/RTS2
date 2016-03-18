a354acb7f06f34d7f650186411e21bed5a8eccb3
Spepsi <floriangardin@hotmail.fr>
2016-03-18 22:59:56 +0100

    ctrl a  suppr

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	// A REGLER \\
 	public static float ratioSpace = 0.7f;
 	public static int framerate = 60;
-	public static int nDelay=4;
+	public static int nDelay=20;
 	///////\\\\\\\\\
 	
 	public static float increment = 0.1f*30/Main.framerate;
