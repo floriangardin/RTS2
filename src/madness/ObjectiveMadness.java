6afb25c31a7fe2009a31942b4e54dcf58eac11ec
Spepsi <floriangardin@hotmail.fr>
2016-09-12 00:20:01 +0100

    ia flo refinement

---
+++ src/madness/ObjectiveMadness.java
@@ -0,0 +1,16 @@
+package madness;
+
+public class ObjectiveMadness {
+	
+	
+	public int reward ; // -1 or +1 (toward madness)
+	
+	
+	
+	public void getReward(){
+		
+	}
+	public void checkIfDone(){
+		
+	}
+}
