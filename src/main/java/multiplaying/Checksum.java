9091ec13edf6b0921a88600ef27ccc074a78a8f1
Florian Gardin <fgardin@quantmetry.com>
2017-09-06 18:17:42 +0200

    change delay and lock on render

--- src/main/java/multiplaying/Checksum.java
+++ src/main/java/multiplaying/Checksum.java
@@ -23,7 +23,12 @@ public class Checksum implements Serializable {
 		boolean[] tab = new boolean[2];
 		tab[0] = (c.round==this.round);
 		tab[1] = (!c.checksum.equals(this.checksum));
-		if(tab[0] && tab[1]){
+		return tab;
+	}
+	public boolean equals(Checksum c){
+		// Checksums that haven't the same round haven't to be compared so return true in this case
+		boolean equalities = c==this || c.round!=this.round || c.checksum.equals(this.checksum);
+		if(!equalities){
 			System.out.println("Desynchro at round : "+c.round);
 			System.out.println(c.checksum);
 			System.out.println(this.checksum);
@@ -31,13 +36,8 @@ public class Checksum implements Serializable {
 				System.out.println("Pas le meme nombre d'unites mon petit gillou");
 			}
 			System.out.println("-------------");
-			
 		}
-		return tab;
-	}
-	public boolean equals(Checksum c){
-		// Checksums that haven't the same round haven't to be compared so return true in this case
-		return c==this || c.round!=this.round || c.checksum.equals(this.checksum);
+		return equalities;
 	}
 	
 	public String toString(){
